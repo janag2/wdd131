6b8d2dd74423abb31d71edbd8f1feebe9bfcca32
janag2 <jesseanaglate2@gmail.com>
2025-05-11 04:45:53 +0000

    updated

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,8 +2,5 @@ var date = new Date();
 var year = date.getFullYear();
 document.getElementById('currentyear').innerHTML = year;
 
-document.getElementById("lastModified").innerHTML = new Date(document.lastModified);
 
-const lastModified = document.querySelector("#lastModified");
-const today = new Date();
-lastModified.innerHTML = `Last Modification: <span class="highlight">${new Intl.DateTimeFormat("en-US", {dateStyle: "short"}).format(today)}</span>`;
+document.querySelector('#lastModified').textContent = `Last Modification: ${document.lastModified}`;
