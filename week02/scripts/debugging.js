8774ec64c736c6869acc7907c312680160a21624
janag2 <jesseanaglate2@gmail.com>
2025-06-05 22:18:32 +0000

    updated

---
+++ week02/scripts/debugging.js
@@ -0,0 +1,16 @@
+//gt elements from the document
+const radiusOutput = document.getElementById('radius');
+const areaOutput = document.getElementById('area');
+
+let area = 0;
+const PI = 3.14159
+
+const radius = 10;
+area = PI * radius * radius;
+radiusOutput = radius;
+areaOutput = area;
+
+radius = 20;
+area = PI * radius * radius;
+radiusOutput = radius;
+areaOutput = area;
