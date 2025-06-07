2c15ba7153d24360c10f5da0ed648fd56a33a9ec
janag2 <jesseanaglate2@gmail.com>
2025-06-07 22:57:06 +0000

    updated

---
+++ scripts/form.js
@@ -0,0 +1,33 @@
+var date = new Date();
+var year = date.getFullYear();
+document.getElementById('currentyear').innerHTML = year;
+
+document.querySelector('#lastModified').textContent = `Last Modification: ${document.lastModified}`;
+
+const products = [
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
+];
